ab2b47f87b747bc80d681cd62c7ab010c951afca
unknown <986402@CS.MUM.EDU>
2018-06-11 16:22:02 -0500

    change

--- lab11/myApp/src/app/app.component.ts
+++ lab11/myApp/src/app/app.component.ts
@@ -3,14 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-counter counter="{{ ComponentCounterValue }}" (counterChange)="handleMe($event)"></app-counter>
+  <app-counter counter="{{ ComponentCounterValue }}" (counterChange)="ComponentCounterValue=$event"></app-counter>
   Component Counter Value = {{ ComponentCounterValue }}
   `,
   styles: [`app-counter{border: 1px solid orange; display: block; width: 220px; padding: 0px 10px; margin-bottom: 10px}`]
 })
 export class AppComponent {
   ComponentCounterValue: number = 5;
-  handleMe(counterVal){
-    this.ComponentCounterValue = counterVal;
-  }
 }
